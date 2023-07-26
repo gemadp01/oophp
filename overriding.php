87035ea139292c9dba99fb73a53dd480f54f255e
Gema Dodi Pranata <gemadp01@gmail.com>
2023-07-26 10:29:18 +0700

    pertemuan7 inheritance dan pertemuan8 overriding

---
+++ overriding.php
@@ -0,0 +1,93 @@
+<?php
+// membuat produk penjualan
+// komik dan game
+
+class Produk
+{
+    public $judul,
+        $penulis,
+        $penerbit,
+        $harga;
+
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+        
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "{$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
+        return $str;
+    }
+}
+
+// Class Komik yang merupakan child dari class Produk (parent)
+class Komik extends Produk
+{
+    public $jmlHalaman;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0) {
+
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
+    public function getInfoProduk()
+    {
+        $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+// Class Game yang merupakan child dari class Produk (parent)
+class Game extends Produk
+{
+    public $waktuMain;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0) {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+        $this->waktuMain = $waktuMain;
+    }
+    public function getInfoProduk()
+    {
+        $str = "Game : " . parent::getInfoProduk() . " (Rp. {$this->harga}) - {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+// Class CetakInfoProduk
+class CetakInfoProduk
+{
+    public function cetak(Produk $produk)
+    {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+
+
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+
+$produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 50);
+
+
+// $produk = new CetakInfoProduk();
+// echo $produk->cetak($produk2);
+
+
+echo $produk1->getInfoProduk();
+echo "<br>";
+echo $produk2->getInfoProduk();
